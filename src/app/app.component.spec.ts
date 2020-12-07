9b3daea3edc594f6407d21a46e5fa92c48cefbe5
cesarsazo <2295156810113@ingenieria.usac.edu.gt>
2020-12-06 23:38:10 -0600

    Initial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'practica1'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('practica1');
+    expect(app.title).toEqual('1234');
   });
 
   it('should render title', () => {
